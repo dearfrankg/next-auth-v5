9fd29fba2fb7ac6f70959088a75a442fa21233f1
Frank Gutierrez <dearfrankg@gmail.com>
2024-05-02 08:42:55 -0700

    activate resend provider

--- src/auth.ts
+++ src/auth.ts
@@ -4,7 +4,7 @@ import { Adapter } from "next-auth/adapters";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import prisma from "./lib/prisma";
-// import Resend from "next-auth/providers/resend";
+import Resend from "next-auth/providers/resend";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
@@ -21,8 +21,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({ allowDangerousEmailAccountLinking: true }),
     GitHub({ allowDangerousEmailAccountLinking: true }),
-    // Resend({
-    //   from: "no-reply@tutorial.codinginflow.com",
-    // }),
+    Resend({
+      from: "no-reply@study-hard.org",
+    }),
   ],
 });
