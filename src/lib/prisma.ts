7bc974d19db968d408d3fd284f0ae23656f04a94
Frank Gutierrez <dearfrankg@gmail.com>
2024-05-31 05:06:24 -0700

    remove neon prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,17 @@
-import { Pool } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@prisma/client";
-
-const prismaClientSingleton = () => {
-  const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
-  const adapter = new PrismaNeon(neon);
-  return new PrismaClient({ adapter });
-};
-
 declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+  var prisma: PrismaClient; // This must be a `var` and not a `let / const`
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+import { PrismaClient } from "@prisma/client";
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
